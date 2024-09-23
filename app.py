c0d14d8bb78629f692b66b0d732bf9e9dc597253
Akash Singh <akashsunilsingh5555@gmail.com>
2024-09-23 12:15:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def main():
     st.markdown("""
     This is an early prototype of using prompting to create o1-like reasoning chains to improve output accuracy. It is not perfect and accuracy has yet to be formally evaluated. It is powered by Groq so that the reasoning step is fast!
                 
-    Open source [repository here](https://github.com/bklieger-groq)
+    Open source [repository here](https://github.com/AK2k30/o1-chatbot)
     """)
     
     # Text input for user query
